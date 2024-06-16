cd5c8c759b654ca49fa43d6088064c780d56bd8d
marvinleonard21 <marvinleonard21@gmail.com>
2024-06-16 19:42:43 +0800

    added FIR Album

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is How I Disappear'));
+app.get('/', (req, res) => res.send('Popular Monster'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
